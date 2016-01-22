d3c04a95bfc7439d2f2d8b7940bec79f30abb186
Alejandro Cirino <cirinoalejandro@gmail.com>
2016-01-22 16:34:02 -0300

    added utility for swagger request handler

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.responseHandlers = require('./lib/http-response-handlers');
 exports.ValidationError = require('./lib/validation-error');
 exports.validateSwaggerSchema = require('./lib/swagger-validate-schema');
+exports.swaggerRequestHandler = require('./lib/swagger-request-handler');
