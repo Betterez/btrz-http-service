59e99c2f0e6b8303bca991ecd7c7dc2654c8399d
Alejandro Cirino <cirinoalejandro@gmail.com>
2016-08-12 14:33:49 -0300

    #5380: move error logging middleware here to reuse in other APIs

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ exports.responseHandlers = require('./lib/http-response-handlers');
 exports.ValidationError = require('./lib/validation-error');
 exports.validateSwaggerSchema = require('./lib/swagger-validate-schema');
 exports.swaggerRequestHandler = require('./lib/swagger-request-handler');
+exports.LogErrorsMiddleware = require('./lib/log-errors-middleware');
