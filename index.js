52cd3a637ac8c4be413318eda927585c7071c8f0
Alejandro Cirino <cirinoalejandro@gmail.com>
2018-10-05 10:43:06 -0300

    #9411: add Paginated Response Builder

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ exports.ValidationError = require('./lib/validation-error');
 exports.validateSwaggerSchema = require('./lib/swagger-validate-schema');
 exports.swaggerRequestHandler = require('./lib/swagger-request-handler');
 exports.LogErrorsMiddleware = require('./lib/log-errors-middleware');
+exports.PaginatedResponseBuilder = require('./lib/paginated-response-builder');
